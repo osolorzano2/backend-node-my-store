9bb1fb1a1d3c1b6646f2e665423e5a1a52a4d131
solorzadev <osolorzano22@gmail.com>
2022-10-25 23:20:42 -0500

    creo que es el segundo commit, la neta no me acuerdo

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("My app")
+const express = require("express");
+const routerApi = require("./routes");
+// const router = require("./routes")
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Holal mi server en express");
+});
+
+app.get("/nueva-ruta", (req, res) => {
+  res.send("Soy la ruta 2");
+});
+
+routerApi(app);
+
+
+// app.get("/users", (req, res) => {
+//   const { limit, offset } = req.query;
+//   if (limit && offset) {
+//     res.json({
+//       limit,
+//       offset,
+//     });
+//   } else {
+//     res.send("No hay parametros");
+//   };
+// });
+
+// app.get("/categories/:categoriId/products/:productId", (req, res) => {
+//   const { categoriId, productId } = req.params;
+//   res.json({
+//     categoriId,
+//     productId,
+//   })
+// })
+
+app.listen(port,  () => {
+  console.log("Mi port " + port)
+});
