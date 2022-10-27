ac66c9a4931b5d6eb3e5d1c09a171ed889754355
solorzadev <osolorzano22@gmail.com>
2022-10-26 22:46:24 -0500

    Mi tercer commit

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,19 +1,12 @@
 const express = require("express");
-const faker = require("faker");
+
+const ProductService = require("./../services/product.service")
 
 const router = express.Router();
+const service = new ProductService();
 
-router.get("/", (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    })
-  }
+router.get("/", async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
 
@@ -24,13 +17,35 @@ router.get("/filter", (req, res) => {
 
 
 // http://localhost:3000/users?limit=10&offset=200
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params;
-  res.json({
-    id,
-    name: "product 3",
-    price: 2000
-  });
+  const product = await service.findOne(id);
+  res.json(product);
+});
+
+router.post("/", async(req, res) => {
+  const body = req.body;
+  const newProduct = await service.create(body);
+  res.status(201).json(newProduct);
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const body = req.body;
+    const product = await service.update(id, body);
+    res.json(product)
+  } catch (error) {
+    res.status(404).json({
+      message: error.message,
+    });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const rta = await service.delete(id);
+  res.json(rta)
 });
 
 module.exports = router;
