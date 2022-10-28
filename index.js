874669527bf66d735956de47d7064c58a732e39a
solorzadev <osolorzano22@gmail.com>
2022-10-27 23:01:58 -0500

    cuarto commit, ya casi acabo el curso pero perdí el rumbo, solo voy a terminar de ver los videos

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routerApi = require("./routes");
-// const router = require("./routes")
+
+const { logErrors, errorHandler, boomErrorHandler } = require("./middlewares/error.handler")
 
 const app = express();
 const port = 3000;
@@ -17,26 +18,9 @@ app.get("/nueva-ruta", (req, res) => {
 
 routerApi(app);
 
-
-// app.get("/users", (req, res) => {
-//   const { limit, offset } = req.query;
-//   if (limit && offset) {
-//     res.json({
-//       limit,
-//       offset,
-//     });
-//   } else {
-//     res.send("No hay parametros");
-//   };
-// });
-
-// app.get("/categories/:categoriId/products/:productId", (req, res) => {
-//   const { categoriId, productId } = req.params;
-//   res.json({
-//     categoriId,
-//     productId,
-//   })
-// })
+app.use(logErrors);
+app.use(boomErrorHandler);
+app.use(errorHandler);
 
 app.listen(port,  () => {
   console.log("Mi port " + port)
--- routes/products.router.js
+++ routes/products.router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const ProductService = require("./../services/product.service")
+const validatorHandler = require("./../middlewares/validator.handler")
+const { createProductSchema, updateProductSchema, getProductSchema} = require("./../schemas/product.schema")
 
 const router = express.Router();
 const service = new ProductService();
@@ -17,28 +19,32 @@ router.get("/filter", (req, res) => {
 
 
 // http://localhost:3000/users?limit=10&offset=200
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  const product = await service.findOne(id);
-  res.json(product);
+router.get("/:id",
+  validatorHandler(getProductSchema, "params"),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
 });
 
 router.post("/", async(req, res) => {
-  const body = req.body;
-  const newProduct = await service.create(body);
-  res.status(201).json(newProduct);
+    const body = req.body;
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
 });
 
-router.patch("/:id", async (req, res) => {
+router.patch("/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
     const body = req.body;
     const product = await service.update(id, body);
     res.json(product)
   } catch (error) {
-    res.status(404).json({
-      message: error.message,
-    });
+    next(error);
   }
 });
 
